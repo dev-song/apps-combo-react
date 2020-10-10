997b158d688b06cd3bd1ab95d42a8f2055765b1e
dev_song <52951039+dev-song@users.noreply.github.com>
2020-10-10 21:41:01 +0900

    Convert Grid component into a class component

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,41 +1,52 @@
 import React from 'react';
 
-function Grid() {
-  const data = [
-    new TableItem('jack', 20, 70),
-    new TableItem('lee', 30, 88),
-    new TableItem('chuck', 17, 75),
-    new TableItem('ralph', 41, 69),
-    new TableItem('mark', 25, 80)
-  ];
+class Grid extends React.Component {
+  constructor(props) {
+    super(props);
+    this.data = [
+      new TableItem('jack', 20, 70),
+      new TableItem('lee', 30, 88),
+      new TableItem('chuck', 17, 75),
+      new TableItem('ralph', 41, 69),
+      new TableItem('mark', 25, 80)
+    ];
 
-  return (
-    <div className='Grid'>
-      <input
-        className='Grid-filter'
-        type='text'
-        placeholder='Please enter keywords'
-      />
-      <table className='Grid-table'>
-        <thead>
-          <tr>
-            <th>name</th>
-            <th>age</th>
-            <th>weight</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map(({ name, age, weight }, index) => (
-            <tr key={index}>
-              <td>{name}</td>
-              <td>{age}</td>
-              <td>{weight}</td>
+    this.state = {
+      data: this.data
+    };
+  }
+
+  render() {
+    const { data } = this.state;
+
+    return (
+      <div className='Grid' >
+        <input
+          className='Grid-filter'
+          type='text'
+          placeholder='Please enter keywords'
+        />
+        <table className='Grid-table'>
+          <thead>
+            <tr>
+              <th>name</th>
+              <th>age</th>
+              <th>weight</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+          </thead>
+          <tbody>
+            {data.map(({ name, age, weight }, index) => (
+              <tr key={index}>
+                <td>{name}</td>
+                <td>{age}</td>
+                <td>{weight}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 class TableItem {
