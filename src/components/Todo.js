488522dfc5dd551734b0aebdc82bb97190fe761b
dev-song <52951039+dev-song@users.noreply.github.com>
2020-10-10 20:33:49 +0900

    Feat: new input field to get user todo data

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,40 @@
 import React from 'react';
 
-function Todo() {
-  return (
-    <div className="Todo">
-    </div>
-  );
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: []
+    }
+  }
+
+  render() {
+    const { todo } = this.state;
+
+    return (
+      <div className='Todo'>
+        {todo.length < 5
+          ?
+          <form className='add-todo__container'>
+            <input
+              type='text'
+              className='add-todo__input'
+              placeholder='What needs to be done?' />
+            <button className='add-todo__button'>+</button>
+          </form>
+          :
+          <p className='message-full-list'>The list is full.</p>
+        }
+      </div>
+    );
+  }
+}
+
+class TodoItem {
+  constructor(text) {
+    this.regTime = new Date().getTime();
+    this.text = text
+  }
 }
 
 export default Todo;
