01fe4b019f08e484e555b9f770b66036e28e3602
dev_song <52951039+dev-song@users.noreply.github.com>
2020-10-10 21:18:29 +0900

    Feat: input field to filter data in grid and table layout

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 function Grid() {
   return (
-    <div className="Grid">
+    <div className='Grid'>
+      <input
+        className='Grid-filter'
+        type='text'
+        placeholder='Please enter keywords'
+      />
+      <table className='Grid-table'>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>age</th>
+            <th>weight</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
     </div>
   );
 }
