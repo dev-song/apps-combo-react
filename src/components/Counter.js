aa4c54f42bd8298a86bd47970867d56fd138528f
dev-song <52951039+dev-song@users.noreply.github.com>
2020-10-12 00:32:03 +0900

    Prepare Counter component before applying redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
-function Counter() {
+function Counter({ number, lastCalc, onIncrement, onDecrement }) {
   return (
     <div className="Counter">
+      <div className="Counter__container">
+        <button
+          className="Counter__increment-button"
+          onClick={onIncrement}
+        >
+          +
+          </button>
+        <h2 className="Counter__number">{number}</h2>
+        <button
+          className="Counter__decrement-button"
+          onClick={onDecrement}
+        >
+          -
+        </button>
+      </div>
+      <p className="Counter__explanation">
+        Count title:
+        <span className="Counter__status"> {lastCalc}</span>
+      </p>
     </div>
   );
 }
