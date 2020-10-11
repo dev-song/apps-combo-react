8e054252026045ae46ed7991e46f62c611e42c71
dev-song <52951039+dev-song@users.noreply.github.com>
2020-10-12 00:32:30 +0900

    Link Redux store to the app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import rootReducer from './store/modules';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__
+  && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
