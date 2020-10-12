9b41f7e2e0fbbcaad3a8d9d959b532c3857f2e60
dev-song <52951039+dev-song@users.noreply.github.com>
2020-10-12 19:16:44 +0900

    Style: Modal component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import './Modal.css';
+
 class Modal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      openModal: false
+      openModal: false,
+      isClosing: false
     };
 
     this.handleOnOff = this.handleOnOff.bind(this);
@@ -13,13 +16,20 @@ class Modal extends React.Component {
   handleOnOff() {
     const delay = this.state.openModal ? 400 : 0;
 
+    if (this.state.openModal) {
+      this.setState({ isClosing: true });
+    }
+
     setTimeout(() => {
-      this.setState({ openModal: !this.state.openModal });
+      this.setState({
+        openModal: !this.state.openModal,
+        isClosing: false
+      });
     }, delay);
   }
 
   render() {
-    const { openModal } = this.state;
+    const { openModal, isClosing } = this.state;
 
     return (
       <div className="Modal">
@@ -32,18 +42,21 @@ class Modal extends React.Component {
 
         {openModal
           ?
-          <div className="Modal__overlay">
-            <h2 className="Modal__overlay--header">Modal Header</h2>
-            <p className="Modal__overlay--body">Modal Body</p>
-            <button
-              className="Modal__overlay--close-button"
-              onClick={this.handleOnOff}
-            >
-              OK
+          <div className={`Modal__overlay${isClosing ? ' closing' : ''}`}>
+            <div className={`Modal__container${isClosing ? ' closing' : ''}`}>
+              <h2 className="Modal__header">Modal Header</h2>
+              <p className="Modal__body">Modal Body</p>
+              <button
+                className="Modal__close-button"
+                onClick={this.handleOnOff}
+              >
+                OK
             </button>
+            </div>
           </div>
           :
-          null}
+          null
+        }
       </div>
     );
   }
