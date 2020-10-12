ac780e496f740cee71f6dbf708dfe2e6b5bc4032
dev-song <52951039+dev-song@users.noreply.github.com>
2020-10-12 11:04:52 +0900

    Feat: getting weather data by cities on Async component

--- src/components/Async.js
+++ src/components/Async.js
@@ -1,10 +1,36 @@
 import React from 'react';
 
-function Async() {
-  return (
-    <div className="Async">
-    </div>
-  );
+class Async extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      weather: null
+    }
+  }
+
+  async getWeather(city) {
+    const weatherApi = `https://abnormal-weather-api.herokuapp.com/cities/search?city=${city}`;
+
+    try {
+      let response = await fetch(weatherApi);
+      let weather = await response.json();
+      this.setState({ weather });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  componentDidMount() {
+    this.getWeather('seoul');
+  }
+
+  render() {
+    return (
+      <div className="Async">
+      </div>
+    );
+  }
 }
 
 export default Async;
