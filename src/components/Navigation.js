cfe16269aed80d5617716686b08751e3e9caf696
dev-song <52951039+dev-song@users.noreply.github.com>
2020-10-12 17:27:43 +0900

    Style: Navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './Navigation.css';
+
 function Navigation({ apps }) {
   return (
     <nav className="Navigation">
-      <ul>
+      <ul className="Navigation__container">
         {
           apps.map(({ name }, index) => (
-            <li key={index}>
-              <Link to={`/${name}`}>{name}</Link>
+            <li
+              key={index}
+              className="Navigation__link--container"
+            >
+              <Link
+                to={`/${name}`}
+                className="Navigation__link"
+              >
+                {name}
+              </Link>
             </li>
           ))
         }
