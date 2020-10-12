17e1a14e9e9ff30ed355c3917d1ad183796e08b9
dev-song <52951039+dev-song@users.noreply.github.com>
2020-10-12 11:19:39 +0900

    Feat: rendering weather data (city, weather, date, image) by cities

--- src/components/Async.js
+++ src/components/Async.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+const CITIES = [
+  'Seoul',
+  'Tokyo',
+  'Beijing',
+  'Toronto'
+];
+
 class Async extends React.Component {
   constructor(props) {
     super(props);
@@ -11,23 +18,52 @@ class Async extends React.Component {
 
   async getWeather(city) {
     const weatherApi = `https://abnormal-weather-api.herokuapp.com/cities/search?city=${city}`;
+    this.setState({ isLoading: true });
 
     try {
       let response = await fetch(weatherApi);
       let weather = await response.json();
-      this.setState({ weather });
+      this.setState({
+        isLoading: false,
+        weather
+      });
     } catch (err) {
       console.error(err);
     }
   }
 
   componentDidMount() {
-    this.getWeather('seoul');
+    this.getWeather(CITIES[0]);
   }
 
   render() {
+    const { isLoading, weather } = this.state;
+
     return (
       <div className="Async">
+        <div className="Async__cities-button">
+          {CITIES.map((city, index) => (
+            <button
+              key={index}
+              onClick={() => this.getWeather(city)}
+            >
+              {city}
+            </button>
+          ))}
+        </div>
+        {!isLoading
+          ?
+          <div className="Async__weather-data">
+            <p>City: {weather.city}</p>
+            <p>Weather: {weather.current.weather}</p>
+            <p>Date: {weather.current.date}</p>
+            <figure>
+              <img src={weather.image_url} alt={`The city of ${weather.city}`} />
+            </figure>
+          </div>
+          :
+          <p>Loading...</p>
+        }
       </div>
     );
   }
