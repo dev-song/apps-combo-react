8ea0f05f13f1a778bf2ed175dc14ef63194e5522
sangsu <sangsu.song@aiceltech.com>
2021-03-24 00:06:42 +0900

    Add a script and a module
    - script is for printing summaries for the test (--verbose argument added)
    - module, jest-dom testing-library, is for handling React DOMs

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
 import Counter from '../components/Counter';
 
 test('renders an app', () => {
   const res = render(<Counter />);
   // console.log(res);
-  expect(screen.getByText('Count title:'));
+  expect(screen.getByText('Count title:')).toBeInTheDocument();
 });
